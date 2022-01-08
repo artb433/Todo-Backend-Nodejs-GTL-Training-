ddb2e427453dafb8740457e85e2a9d290ac919fe
Art b <94374097+artb433@users.noreply.github.com>
2022-01-08 01:09:51 +0000

    my

--- express_server.js
+++ express_server.js
@@ -1,9 +1,9 @@
-
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const todo_controller = require('./controllers/todo_controller');
 const mongoose = require ('mongoose');
-require('dotenv').config();
+
 const PORT = process.env.PORT || 6000;
 
 app.use(express.json());
