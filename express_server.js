0e67a3208e375749664efadd0b298345d1c52610
Art b <94374097+artb433@users.noreply.github.com>
2022-01-13 22:50:36 +0000

    my

--- express_server.js
+++ express_server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 6000;
 app.use(express.json());
 app.get('/',function(req,res){
     res.status(200).json({message: 'Welcome to Art.b API with GTL'});
-})
+});
 app.get('/todos',todo_controller.getAllTodos);
 app.post('/todos',todo_controller.addTodo);
 app.patch('/todos/:todoId',todo_controller.updateTodoById);
@@ -18,7 +18,10 @@ app.get('/todos/:todoId', todo_controller.getTodoById);
 
 app.listen(PORT, function(){
 console.log(`App has started to run, listening to port ${PORT}`);
-mongoose.connect(process.env.DB_URL)
+mongoose.connect(process.env.DB_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology:true,
+})
 .then(function(){
  console.log('Success: Connected to you DB');
 })
